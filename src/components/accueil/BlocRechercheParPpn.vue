4457bf98214fde1c57fef80661b5a3ea63e2b051
SamuelQuetin <quetin@abes.fr>
2025-10-21 11:57:41 +0200

    Fix BlocRechercheParPpn.vue fix HMR

--- src/components/accueil/BlocRechercheParPpn.vue
+++ src/components/accueil/BlocRechercheParPpn.vue
@@ -4,27 +4,26 @@
       Pour optimiser l'analyse, il est recommandé de ne pas soumettre plus de 5000 PPN en une seule fois
     </span>
     <v-combobox
-        label="Saisir ou coller des PPN, puis cliquer hors du cadre (ou appuyer sur Entrée)"
         v-model="ppnListCombobox"
         v-model:search="lastValuesTypedOrPasted"
         append-inner-icon=""
         class="pt-1 px-1 pb-0 mb-0"
         data-cy="COMBOBOX_AJOUT_PPN"
-        filled
+        label="Saisir ou coller des PPN, puis cliquer hors du cadre (ou appuyer sur Entrée)"
         multiple
-        dense
+        variant="filled"
+        @update:search="clearModel"
         @blur="checkValuesAndFeedPpnListTyped"
         @keydown.enter="checkValuesAndFeedPpnListTyped"
-        @keydown.tab="checkValuesAndFeedPpnListTyped"
     >
       <template v-slot:selection="data">
         <v-chip
             v-if="data.item.value === ppnCopied"
             :key="JSON.stringify(data.item.value)"
-            v-bind="data.attrs"
             aria-label="PPN copié"
             color="#eafaed"
             role="img"
+            v-bind="data.attrs"
             @click="copyLabelItem(data.item.value)"
             @click:close="removeItem(data.item.value)"
         >
@@ -36,12 +35,12 @@
         <v-chip
             v-else
             :key="JSON.stringify(data.item.value)"
-            v-bind="data.attrs"
             :disabled="data.disabled"
             :model-value="data.selected"
+            closable
+            v-bind="data.attrs"
             @click="copyLabelItem(data.item.value)"
             @click:close="removeItem(data.item.value)"
-            closable
         >
           <div :aria-label="'PPN numéro : ' + data.item.title + '. Cliquez pour copier ce numéro de PPN'" role="img">
             <span class="pr-2"> {{ data.item.title }}</span>
@@ -51,16 +50,16 @@
     </v-combobox>
     <v-sheet class="d-flex align-end flex-column pt-0 pr-1" style="margin-top: -23px">
       <v-btn
+          append-icon="mdi-delete"
           class="pr-1 pt-0 mt-0 elevation-0"
+          color="#4D4D4D"
           data-cy="del_all_ppn"
           depressed
-          size="small"
-          append-icon="mdi-delete"
-          tile
           flat
+          size="small"
+          tile
           variant="outlined"
           @click="removeAllItems"
-          color="#4D4D4D"
       >
         Vider la liste de ppn
       </v-btn>
@@ -79,9 +78,6 @@
       </div>
     </v-sheet>
     <v-file-input
-        @dragleave="dragLeave"
-        @drop.prevent="dropFile"
-        @dragover.prevent="dragOver"
         ref="fileInput"
         v-model="fichierLoaded"
         :clearable="false"
@@ -99,14 +95,17 @@
         show-size
         truncate-length=75
         type="file"
-        @change="isAllowToSend">
+        @change="isAllowToSend"
+        @dragleave="dragLeave"
+        @drop.prevent="dropFile"
+        @dragover.prevent="dragOver">
     </v-file-input>
     <v-alert v-if="analyseStore.getPpnInvalidsList.length !== 0"
              border="start"
              elevation="2"
              type="error"
              variant="outlined"
-            >
+    >
       Les PPN listés ci-dessous présentent une syntaxe non conforme et ne seront pas analysés :<br>
       <span style="color: #595959; font-size: small">Rappel : syntaxe d'un PPN = 9 caractères, composés de 9 chiffres ou de 8 chiffres + la lettre X</span><br>
       <v-expansion-panels>
@@ -144,7 +143,7 @@
 
 <script setup>
 import {useAnalyseStore} from "@/stores/analyse";
-import {onUpdated, ref} from 'vue';
+import { onUpdated, ref} from 'vue';
 import {useHistoriqueStore} from "@/stores/historique";
 import {useRoute} from 'vue-router'
 
@@ -177,6 +176,9 @@ const snackbarCopyPpnNumberStatus = ref(false);
 const snackbarMessage = ref('');
 const ppnCopied = ref('');
 
+
+const isValidPpn = ppn => /^\d{8}(\d|X|x)$/.test(ppn);
+
 onUpdated(() => {
   if (ppnListCombobox.value.length > 0 && lastValuesTypedOrPasted.value === '') {
     analyseStore.setPpnValidsList(ppnListCombobox.value); //Alimentation du store avec les ppn valides
@@ -184,6 +186,10 @@ onUpdated(() => {
     emitOnEvent();
   }
 });
+
+function clearModel() {
+  ppnListCombobox.value = ppnListCombobox.value.filter(isValidPpn)
+}
 
 function dropFile(dropObject) {
   let filesDragged = [];
@@ -218,7 +224,7 @@ function copyLabelItem(item) {
  * Fonction qui permet l'affichage de la snackbar en cas de copie de la liste des ppn à la syntaxe erronée
  */
 function copyPnnWrongSyntax() {
-  navigator.clipboard.writeText(ppnInvalids.value)
+  navigator.clipboard.writeText(ppnInvalids.value.join(","))
   snackbarMessage.value = "Les PPN avec syntaxe erronée ont été copié dans le presse papier";
   snackbarCopyPpnNumberStatus.value = true;
 }
@@ -253,30 +259,38 @@ function removeAllItems() {
  * Contrôle des chaînes de caractères saisies dans la combobox à la sortie de la souris du champ et alimentation de ppnListTyped
  */
 function checkValuesAndFeedPpnListTyped() {
-  if (lastValuesTypedOrPasted.value) {
-    //Si la valeur n'est pas nulle, ce qui se produit si l'utilisateur sort du cadre sans rien taper
-    ppnListCombobox.value.pop()
-    let arrayWithValidsPpn = lastValuesTypedOrPasted.value.split(/[^\da-zA-Z]/).filter(ppn_to_check => ppn_to_check.match(/^(\d{8}(\d|X|x))$/));
-    let arrayWithInvalidsPpn = lastValuesTypedOrPasted.value.split(/[^\da-zA-Z]/).filter(ppn_to_check => !ppn_to_check.match(/^(\d{8}(\d|X|x))$/)).filter(str_to_clean => str_to_clean.trim() !== '');
-    let arrayWithValidsPpnWithUniqueValues = arrayWithValidsPpn.filter((v, i, a) => a.indexOf(v) === i); //Fonction anonyme de dédoublonnage sur la saisie en cours
-    let arrayWithInvalidsPpnWithUniqueValues = arrayWithInvalidsPpn.filter((v, i, a) => a.indexOf(v) === i); //Fonction anonyme de dédoublonnage sur la saisie en cours
-    //Ppn valides
-    arrayWithValidsPpnWithUniqueValues.forEach(currentValidPpn => {
-      ppnListCombobox.value.push(currentValidPpn)
-    });
-    ppnListCombobox.value = ppnListCombobox.value.filter(function (item, index, inputArray) {
-      return inputArray.indexOf(item) === index;
-    }); //Supprime les ppn qui serait en doublon sur une saisie précédente
-    //Ppn invalide
-    arrayWithInvalidsPpnWithUniqueValues.forEach(currentValidPpn => ppnInvalids.value.push(currentValidPpn));
-    ppnInvalids.value = ppnInvalids.value.filter(function (item, index, inputArray) {
-      return inputArray.indexOf(item) === index;
-    }); //Supprime les ppn qui serait en doublon sur une saisie précédente
-  }
-  analyseStore.setPpnValidsList(ppnListCombobox.value); //Alimentation du store avec les ppn valides
-  analyseStore.setPpnInvalidsList(ppnInvalids.value); //Alimentation du store avec les ppn invalides
-  lastValuesTypedOrPasted.value = ''; //On vide la chaîne puisqu'on à alimenté les valeurs valides dans :value="ppnListCombobox"
+  const rawInput = lastValuesTypedOrPasted.value;
+  if (!rawInput) return; // Rien à faire si le champ est vide
+
+  const tokens = rawInput.split(/[^\da-zA-Z]+/).map(s => s.trim()).filter(Boolean);
+
+  const valids = [...new Set(tokens.filter(isValidPpn))];
+  const invalids = [...new Set(tokens.filter(ppn => !isValidPpn(ppn)))];
+
+  // Ajout des PPN valides sans doublon
+  ppnListCombobox.value = [...new Set([...ppnListCombobox.value, ...valids])];
+
+  // Ajout des PPN invalides sans doublon
+  ppnInvalids.value = [...new Set([...ppnInvalids.value, ...invalids])];
+
+  isValidPpnInPpnListCombobox(rawInput);
+
+  // Mise à jour du store
+  analyseStore.setPpnValidsList(ppnListCombobox.value);
+  analyseStore.setPpnInvalidsList(ppnInvalids.value);
+
+  // Réinitialisation de la saisie
+  lastValuesTypedOrPasted.value = '';
+
   emitOnEvent();
+}
+
+function isValidPpnInPpnListCombobox(input) {
+  // Retire la chaîne complète si elle se trouve dans la liste des PPN (cas d’un seul PPN saisi)
+  if (!isValidPpn(input)) {
+    const index = ppnListCombobox.value.indexOf(input);
+    if (index !== -1) ppnListCombobox.value.splice(index, 1);
+  }
 }
 
 /**
