6843a25062ccd4479f4cb4a0a5478fa7b841ead5
SamuelQuetin <quetin@abes.fr>
2025-10-31 09:17:44 +0100

    forgoten file

---
+++ src/components/resultats/KeyboardNavigation.vue
@@ -0,0 +1,43 @@
+<script setup>
+
+import { onMounted, onUnmounted } from 'vue'
+function handleKeydown(e) {
+  switch (e.key) {
+    case 'ArrowUp':
+    case 'ArrowLeft':
+      e.preventDefault()
+      previousSelectedItem()
+      break;
+    case 'ArrowDown':
+    case 'ArrowRight':
+      e.preventDefault()
+      nextSelectedItem()
+      break;
+  }
+}
+onMounted(() => {
+  window.addEventListener('keydown', handleKeydown)
+})
+onUnmounted(() => {
+  window.removeEventListener('keydown', handleKeydown)
+})
+
+const currentPpn = defineModel('currentPpn', {type: String});
+const props = defineProps({itemsSortedAndFiltered: Array });
+
+
+function nextSelectedItem() {
+  let index = props.itemsSortedAndFiltered.findIndex(item => item.ppn === currentPpn.value);
+  if (index < props.itemsSortedAndFiltered.length - 1) {
+    currentPpn.value = props.itemsSortedAndFiltered[index + 1].ppn;
+  }
+}
+
+function previousSelectedItem() {
+  let index = props.itemsSortedAndFiltered.findIndex(item => item.ppn === currentPpn.value);
+  if (index > 0) {
+    currentPpn.value = props.itemsSortedAndFiltered[index - 1].ppn;
+  }
+}
+
+</script>
